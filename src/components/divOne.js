f7549296f9756e18ca3fc6202f35be6bd787681f
tathagata218 <tathagata218@yahoo.com>
2017-12-14 15:08:35 -0600

    this is the stuff

--- src/components/divOne.js
+++ src/components/divOne.js
@@ -3,7 +3,7 @@ import "./divOne.css";
 class CompOne extends Component{
 render (){
     return (
-    <div className="main">
+    <div >
         <div className="divOne mainDiv"></div>
         <div className="divTwo mainDiv"></div>
         <div className="divThree mainDiv"></div>
